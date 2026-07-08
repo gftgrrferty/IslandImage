f5aa55b5b9a0f39d164db3e6cb3cd6bca11adb95
gftgrrferty <gftgrrferty@gmail.com>
2026-07-08 09:47:30 +0900

    プロジェクト作成

---
+++ Island Image/Island Image/Island_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Island_ImageApp.swift
+//  Island Image
+//
+//  Created by gftgrrferty on 2026/07/08.
+//
+
+import SwiftUI
+
+@main
+struct Island_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
