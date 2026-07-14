28610ac946026597c6b53a046f2edc048d32e4cb
gftgrrferty <gftgrrferty@gmail.com>
2026-07-14 23:19:07 +0900

    画像を保存し表示できるようにした

--- Island Image/Island Image/ContentView.swift
+++ Island Image/Island Image/ContentView.swift
@@ -5,29 +5,52 @@
 //  Created by gftgrrferty on 2026/07/08.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    let manager = NotesManager()
-    @State var notes: [NoteData] = []
+    private let manager = NotesManager()
+    @State private var pickerItem: PhotosPickerItem?
+    @State private var notes: [NoteData] = []
     
     var body: some View {
         NavigationStack {
             List {
                 ForEach($notes, id: \.id) { $note in
-                    TextField("ノートを入力", text: $note.text)
+                    VStack {
+                        TextField("ノートを入力", text: $note.text)
+                        // 画像を表示
+                        if let imageURL = note.getImageURL() {
+                            // 画像を非同期で表示する
+                            AsyncImage(url: imageURL) { image in
+                                //
+                                image.image?
+                                    .resizable()
+                                    .scaledToFit()
+                            }
+                        }
+                    }
                 }
             }
             .toolbar {
-                Button(action: {
-                    notes.append(NoteData())
-                }) {
+                PhotosPicker(selection: $pickerItem, matching: .images) {
                     Label("追加", systemImage: "plus")
                 }
             }
         }
         .onChange(of: notes) {
             manager.saveNotes(notes)
+        }
+        // 画像が選択されたら新しいノートを作成して画像を保存する。
+        .onChange(of: pickerItem) {
+            // ここでpickerItemがnilでないことを確定させる
+            guard let pickerItem else { return }
+            var newNote = NoteData()
+            // 画像の保存を待ってからノートを保存する非同期処理。
+            Task {
+                await newNote.saveImage(pickerItem)
+                notes.append(newNote)
+            }
         }
         // NavigationStackが表示された時に実行される。
         .onAppear {
--- Island Image/Island Image/NoteData.swift
+++ Island Image/Island Image/NoteData.swift
@@ -12,13 +12,67 @@ import SwiftUI
 struct NoteData: Codable, Equatable, Identifiable {
     var id: UUID = UUID()
     var text = ""
-    var image: Data?
+    var image: String?
     
-    func saveImage(_ target: PhotosPickerItem?) {
-        // targetから画像データを取り出す
-        // 最大128pxくらいにリサイズする
-        // self.imageに取り込む
-        // self.image = ???
+    // 画像を保存する非同期関数
+    mutating func saveImage(_ item: PhotosPickerItem) async {
+        // 画像をData型にする
+        guard let data = try? await item.loadTransferable(type: Data.self) else { return }
+        // App Groupの共有のディレクトリのパスを取得
+        guard var itemURL = FileManager.default.containerURL(
+            forSecurityApplicationGroupIdentifier: "group.net.abidaze.Island-Image"
+        ) else {
+            return
+        }
+        
+        // 画像を保存するディレクトリのパスを追加
+        itemURL.appendPathComponent("note_image", isDirectory: true)
+        // 画像を保存するディレクトリがなかったら作成する
+        if !FileManager.default.fileExists(atPath: itemURL.path) {
+            do {
+                try FileManager.default.createDirectory(at: itemURL, withIntermediateDirectories: true)
+            } catch {
+                return
+            }
+        }
+        // 画像のファイル名をノートのUUIDにする
+        itemURL.appendPathComponent(self.id.uuidString)
+        
+        // 画像の拡張子を判断する
+        let ext: String
+        switch item.supportedContentTypes.first {
+        case .png:
+            ext = "png"
+        case .jpeg:
+            ext = "jpg"
+        case .heic:
+            ext = "heic"
+        default:
+            return
+        }
+        // 判断した拡張子をファイル名に追加
+        itemURL.appendPathExtension(ext)
+        // ファイルを作成
+        do {
+            try data.write(to: itemURL)
+        } catch {
+            return
+        }
+        // ファイル名を保存
+        self.image = itemURL.lastPathComponent
+        print(itemURL)
+    }
+    
+    func getImageURL() -> URL? {
+        guard let image = self.image else { return nil }
+        guard var itemURL = FileManager.default.containerURL(
+            forSecurityApplicationGroupIdentifier: "group.net.abidaze.Island-Image"
+        ) else {
+            return nil
+        }
+        itemURL.appendPathComponent("note_image", isDirectory: true)
+        itemURL.appendPathComponent(image)
+        return itemURL
     }
     
 //    func uiImage() -> UIImage? {
