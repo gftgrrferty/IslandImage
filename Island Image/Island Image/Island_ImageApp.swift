64c8e205a7f92fa54a7d4f237a84b8dc913f880c
Cizzuk <dropcat@cizzuk.net>
2026-07-15 10:07:00 +0900

    CizzukのTeamで開発するをための設定

--- Island Image/Island Image/Island_ImageApp.swift
+++ Island Image/Island Image/Island_ImageApp.swift
@@ -7,7 +7,11 @@
 
 import SwiftUI
 
+#if DEBUG_DROPCAT
+let appGroupID: String = "group.net.cizzuk.test.net.abidaze.Island-Image"
+#else
 let appGroupID: String = "group.net.abidaze.Island-Image"
+#endif
 
 @main
 struct Island_ImageApp: App {
