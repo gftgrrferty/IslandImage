7f81822edec2a84b73ef40e0ab06a4bf37c71be0
Cizzuk <dropcat@cizzuk.net>
2026-07-15 10:12:07 +0900

    WidgetExtensionを作成

---
+++ Island Image/WidgetExtension/WidgetExtensionBundle.swift
@@ -0,0 +1,15 @@
+//
+//  WidgetExtensionBundle.swift
+//  WidgetExtension
+//
+//  Created by Cizzuk on 2026/07/15.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetExtensionBundle: WidgetBundle {
+    var body: some Widget {
+    }
+}
